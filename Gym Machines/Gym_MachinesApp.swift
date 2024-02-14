2b024880160763cfe637d2a3e1ef0605fe82afd1
Dominique Del Valle <dominique.delvalle@synaptic.cl>
2024-02-13 22:49:57 -0300

    Initial Commit

---
+++ Gym Machines/Gym_MachinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_MachinesApp.swift
+//  Gym Machines
+//
+//  Created by Dominique Del Valle on 13-02-24.
+//
+
+import SwiftUI
+
+@main
+struct Gym_MachinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
